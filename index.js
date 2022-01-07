686f5ff276ff53094ea6bb3381afc2b0b37af1b7
Justin Luciano <justinlluciano@gmail.com>
2022-01-06 16:24:10 -0800

    commit tree ee2f831b23cff5c115fc551df3ebe98740c237ac

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+function createEmployeeRecord(array) {
+  const employee = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employee;
+}
+
+function createEmployeeRecords(arrays) {
+  const records = [];
+  arrays.forEach((array) => records.push(createEmployeeRecord(array)));
+  return records;
+}
+
+function createTimeInEvent(record, timeDate) {
+  const timeDateArray = timeDate.split(" ");
+  console.log(timeDateArray);
+  record.timeInEvents.push({
+    type: "TimeIn",
+    date: timeDateArray[0],
+    hour: parseInt(timeDateArray[1]),
+  });
+  return record;
+}
+
+function createTimeOutEvent(record, timeDate) {
+  const timeDateArray = timeDate.split(" ");
+  console.log(timeDateArray);
+  record.timeOutEvents.push({
+    type: "TimeOut",
+    date: timeDateArray[0],
+    hour: parseInt(timeDateArray[1]),
+  });
+  return record;
+}
+
+function hoursWorkedOnDate(record, date) {
+  const timeIn = record.timeInEvents.find((event) => event.date === date);
+  const timeOut = record.timeOutEvents.find((event) => event.date === date);
+  return (parseInt(timeOut.hour) - parseInt(timeIn.hour)) / 100;
+}
+
+function wagesEarnedOnDate(record, date) {
+  return record.payPerHour * hoursWorkedOnDate(record, date);
+}
